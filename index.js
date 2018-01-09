8bedc7fd17200f9b91986a14f9d34b7d91b03664
getta <mulugetarobertson5@gmail.com>
2018-01-09 20:26:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a *b; 
+}
+function divide(a,b){
+  return a/b;
+}
+function inc(n){
+  return n += 1; 
+}
+function dec(n)
+{
+  return n -= 1; 
+}
+function makeInt(n)
+{
+  parseInt("20202",10)
+  return n
+}
+ function makeInt(n){
+   parseInt("0" , 10);
+   return n; 
+   
+ }
